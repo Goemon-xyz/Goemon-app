09d757e8d74b57564df8f36239b6ab44098f0113
Shauryacious <shauryaagrawal2718@gmail.com>
2024-06-13 20:57:43 +0530

    Implemented wallet balance section with functions like visibility and change of color on profit or loss

--- app/(tabs)/portfolio/index.tsx
+++ app/(tabs)/portfolio/index.tsx
@@ -1,16 +1,17 @@
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import { ThemedText } from '@/components/ThemedText'
+import { ThemedView } from '@/components/ThemedView'
+import WalletBalanceSection from '@/components/ui/home/wallet-balance-section'
+import { View, Text, SafeAreaView } from 'react-native'
 
 function Portfolio() {
   return (
     <SafeAreaView>
       <ThemedView>
         <ThemedText>portfolio</ThemedText>
+        <WalletBalanceSection />
       </ThemedView>
     </SafeAreaView>
-  );
+  )
 }
 
-export default Portfolio;
+export default Portfolio
