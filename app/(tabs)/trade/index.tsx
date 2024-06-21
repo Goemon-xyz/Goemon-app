4bf8660a2794514182469c0218307a63a80d667e
Shauryacious <shauryaagrawal2718@gmail.com>
2024-06-22 00:42:41 +0530

    Completed Section

--- app/(tabs)/trade/index.tsx
+++ app/(tabs)/trade/index.tsx
@@ -1,5 +1,6 @@
 import { ThemedText } from '@/components/ThemedText'
 import { ThemedView } from '@/components/ThemedView'
+import SelectPairAndSeeRatesSection from '@/components/tabs/trade/trade-execution-page/select-pair-and-see-rates'
 import HistoryAndHotSection from '@/components/ui/home/history-hot-section'
 import React from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
@@ -7,7 +8,7 @@ import { View, Text, SafeAreaView } from 'react-native'
 function Trade() {
   return (
     <SafeAreaView>
-      <HistoryAndHotSection />
+      <SelectPairAndSeeRatesSection />
     </SafeAreaView>
   )
 }
